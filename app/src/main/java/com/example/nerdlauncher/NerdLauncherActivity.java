ad9f99ee580c83dbef9bab99befe62b719ed1659
prateekarora090 <prateek_arora@alumni.brown.edu>
2020-10-02 18:56:24 -0400

    changes for chap 24

--- app/src/main/java/com/example/nerdlauncher/NerdLauncherActivity.java
+++ app/src/main/java/com/example/nerdlauncher/NerdLauncherActivity.java
@@ -1,16 +1,10 @@
 package com.example.nerdlauncher;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NerdLauncherActivity extends SingleFragmentActivity {
+import androidx.fragment.app.Fragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class NerdLauncherActivity extends SingleFragmentActivity {
 
     @Override
     public Fragment createFragment() {
